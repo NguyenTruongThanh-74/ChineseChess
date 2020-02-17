581d5164ab63fc446cf3c259ee7cd8e8ad8939d0
dung <matdieuhau999@gmail.com>
2020-02-17 19:31:01 +0700

    convert to maven project

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        ChessGame game = new ChessGame();
+        game.init();
+        game.run();
+    }
+}
